066d4b6b75736e44445045fb863c403d28879b46
homeautoak-svg <homeauto.ak@gmail.com>
2026-08-14 10:00:20 +0200

    Update eleventy.config.js

--- eleventy.config.js
+++ eleventy.config.js
@@ -2,7 +2,8 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy({ "public/css": "css" });
   eleventyConfig.addPassthroughCopy({ "public/images": "images" });
   eleventyConfig.addPassthroughCopy({ "public/admin": "admin" });
-
+  eleventyConfig.addPassthroughCopy("src/favicon.svg");
+  
   eleventyConfig.addCollection("posts", (collectionApi) => {
     return collectionApi.getFilteredByGlob("src/posts/*.md").sort((a, b) => {
       return b.data.datum_sort - a.data.datum_sort;
